fa63133b357c09a575fa53f3a649613f98f4aee4
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-05 16:20:23 +0200

    Add Comment Form component for adding new comments

---
+++ src/components/CommentForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+
+export const CommentForm = ({postId, setComments}) =>{
+    const [name, setName] = useState('')
+    const [comment, setComment] = useState('')
+    const [error, setError] = useState('')
+
+
+    const addComment = async (e) =>{
+        e.preventDefault()
+        const res = await fetch('/comments/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+             body: JSON.stringify({username: name, desc: comment, id: postId}) 
+        })
+        const json = await res.json()
+
+        if(res.ok){
+            setComments(prevComments=>[json, ...prevComments])
+            setError('')
+            setName('')
+            setComment('')
+        }else{
+            setError(json.error)
+        }
+
+
+    }
+
+
+    return  (
+        <div style={{backgroundColor: '#3f3f46'}}>
+            <form onSubmit={addComment} className="form-comment">
+                <h4>Enter your name</h4>
+                <input onChange={(e)=>setName(e.target.value)} value={name}/> 
+                <input onChange={(e)=>setComment(e.target.value)} placeholder="Add comment..." value={comment} />
+                {<h4 style={{color: 'red'}}>{error}</h4>}
+                <button>Add comment</button>
+            </form>
+
+
+        </div>
+
+
+
+    )
+
+
+}
