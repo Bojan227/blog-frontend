b568731d3159101ac878ebfcba759354744f77b6
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-02-24 16:15:20 +0100

    update p tags text

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -62,7 +62,6 @@ export const PostDetails = () => {
             display: 'flex',
             gap: '10px',
             flexDirection: 'column',
-            padding: '20px',
           }}
         >
           {parse(description)}
