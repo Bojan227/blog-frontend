a4d329dce2046569447df980b1492361f1c5bce5
Bojan227 <blazevski.bojan1@gmail.com>
2023-04-01 19:16:53 +0200

    remove portfolio tab

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import React, { useState, useContext } from 'react';
-import theme from '../images/theme-light-dark.png';
-import github from '../images/github.png';
-import linkedin from '../images/linkedin.png';
-import twitter from '../images/twitter.png';
-import menu from '../images/menu.png';
-import { ThemeContext } from '../App';
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import React, { useState, useContext } from "react";
+import theme from "../images/theme-light-dark.png";
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png";
+import twitter from "../images/twitter.png";
+import menu from "../images/menu.png";
+import { ThemeContext } from "../App";
 
 const Nav = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -14,7 +14,7 @@ const Nav = () => {
 
   const variants = {
     open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: '-100%' },
+    closed: { opacity: 0, x: "-100%" },
   };
 
   return (
@@ -23,7 +23,7 @@ const Nav = () => {
         <li onClick={() => setToggleMenu(!toggleMenu)} className="menu">
           <img src={menu} alt="menu" />
         </li>
-        <Link to="/" style={{ color: 'black' }}>
+        <Link to="/" style={{ color: "black" }}>
           <h1>Boki's Blog</h1>
         </Link>
 
@@ -35,10 +35,8 @@ const Nav = () => {
       <ul className="main-navigation">
         <div>
           <Link to="/">
-            <li style={{ color: 'black' }}>Home</li>
+            <li style={{ color: "black" }}>Home</li>
           </Link>
-
-          <li>Portfolio</li>
         </div>
         <div className="icons-container">
           <a
@@ -71,21 +69,21 @@ const Nav = () => {
         </div>
       </ul>
       <motion.div
-        animate={toggleMenu ? 'open' : 'closed'}
+        animate={toggleMenu ? "open" : "closed"}
         variants={variants}
         className="side-menu"
       >
         <div>
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
             <Link to="/">
               <li
-                style={{ color: 'black' }}
+                style={{ color: "black" }}
                 onClick={() => setToggleMenu(!toggleMenu)}
               >
                 Home
@@ -93,18 +91,17 @@ const Nav = () => {
             </Link>
             <button
               style={{
-                marginRight: '55px',
-                backgroundColor: 'inherit',
-                border: '0',
-                fontSize: '1.5rem',
-                cursor: 'pointer',
+                marginRight: "55px",
+                backgroundColor: "inherit",
+                border: "0",
+                fontSize: "1.5rem",
+                cursor: "pointer",
               }}
               onClick={() => setToggleMenu(!toggleMenu)}
             >
               X
             </button>
           </div>
-          <li>Portfolio</li>
         </div>
       </motion.div>
     </nav>
